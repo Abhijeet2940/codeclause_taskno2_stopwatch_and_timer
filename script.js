229285fcb0d97ba89198496baccb910387b75bfe
Abhijeet2940 <88205898+Abhijeet2940@users.noreply.github.com>
2023-06-30 17:30:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+let timerDisplay = document.querySelector('.timerDisplay');
+let stopBtn = document.getElementById('stopBtn');
+let startBtn = document.getElementById('startBtn');
+let resetBtn = document.getElementById('resetBtn');
+
+let msec = 00;
+let secs = 00;
+let mins = 00;
+
+let timerId = null;
+
+startBtn.addEventListener('click', function(){
+    if(timerId !== null){
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+});
+
+stopBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+});
+
+resetBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+    timerDisplay.innerHTML = `00 : 00 : 00`;
+    msec = secs = mins = 00;
+});
+
+function startTimer(){
+    msec++;
+    if(msec == 100){
+        msec = 0;
+        secs++;
+        if(secs == 60){
+            secs = 0;
+mins++;
+        }
+    }
+
+    let msecString = msec < 10 ? `0${msec}` : msec;
+    let secsString = secs < 10 ? `0${secs}` : secs;
+    let minsString = mins < 10 ? `0${mins}` : mins;
+    
+
+    timerDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
+
+}
+const endDate = "25 July 2023 08:20:00 PM"
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input")
+   
+function clock() {
+    const end = new Date(endDate)
+    const now = new Date()
+    const diff = (end - now) / 1000;
+
+    if (diff < 0) return;
+
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+}
+
+
+clock()
+
+
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
